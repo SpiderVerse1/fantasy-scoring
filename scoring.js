f096e235151f8a5bd7d11ca44766429d8c582bc5
Mark Royer <mtroyer7@yahoo.com>
2020-03-17 12:53:28 -0400

    Changed scoring a bit

--- scoring.js
+++ scoring.js
@@ -1,13 +1,13 @@
-function calculateScore(player) {
+const calculateScore = (player) => {
   switch (player.position) {
     case 'QB':
-      return PassingScore(player) + RushingScore(player)
+      return passingScore(player) + rushingScore(player)
     case 'RB':
-      return RushingScore(player) + ReceivingScore(player) + ReturnScore(player)
+      return rushingScore(player) + receivingScore(player) + returnScore(player)
     case 'WR':
-      return RushingScore(player) + ReceivingScore(player) + ReturnScore(player)
+      return rushingScore(player) + receivingScore(player) + returnScore(player)
     case 'TE':
-      return ReceivingScore(player)
+      return receivingScore(player)
     case 'K':
       return 0
   }
@@ -15,19 +15,19 @@ function calculateScore(player) {
 //1 point for every 25 yards passing, 6 points for every passing touchdown, 
 //-3 points for every interception, 1 point for every 10 yards rushing, 6 points for every rushing touchdown,
 // -3 points for every rushing fumble
-function PassingScore(player) {
+const passingScore = (player) => {
   const yards = player.stats.passing.yards / 25
   const interceptions = player.stats.passing.interceptions * -3
-  const touchdowns = player.stats.passing.touchdowns * 6
+  const touchDowns = player.stats.passing.touchdowns * 6
 
-  return yards + interceptions + touchdowns 
+  return yards + interceptions + touchDowns 
 }
 
-function RushingScore(player) {
+const rushingScore = (player) => {
   const yards = player.stats.rushing.yards / 10
-  const touchdowns = player.stats.rushing.touchdowns * 6
+  const touchDowns = player.stats.rushing.touchdowns * 6
   const fumbles = player.stats.rushing.fumbles * -3
-  return yards + touchdowns + fumbles
+  return yards + touchDowns + fumbles
 }
 //1 point for every 10 yards rushing, 6 points for every rushing touchdown, -3 points for every rushing fumble,
 // 1 point for every reception, 1 point for every 10 yards receiving, 6 points for every receiving touchdown, 
@@ -35,24 +35,24 @@ function RushingScore(player) {
 // -3 points for every kick return fumble, 1 point for every 15 punt return yards, 6 points for every punt return touchdown, 
 //-3 points for every punt return fumble
 
-function ReceivingScore(player) {
+const receivingScore = (player) => {
   const yards = player.stats.receiving.yards / 10
   const fumbles = player.stats.receiving.fumbles * -3
-  const touchdowns = player.stats.receiving.touchdowns * 6
+  const touchDowns = player.stats.receiving.touchdowns * 6
   const receptions = player.stats.receiving.receptions 
 
-  return receptions + yards + touchdowns + fumbles
+  return receptions + yards + touchDowns + fumbles
 }
 
-function ReturnScore(player) {
+const returnScore = (player) => {
   const kickYards = player.stats.return.kickreturn.yards / 15
   const puntYards = player.stats.return.puntreturn.yards / 15
-  const punttouchdowns = player.stats.return.puntreturn.touchdowns * 6
-  const kicktouchdowns = player.stats.return.kickreturn.touchdowns * 6
+  const puntTouchdowns = player.stats.return.puntreturn.touchdowns * 6
+  const kickTouchdowns = player.stats.return.kickreturn.touchdowns * 6
   const puntFumbles = player.stats.return.puntreturn.fumbles * -3
   const kickFumbles = player.stats.return.kickreturn.fumbles * -3
 
-  return  kickYards + puntYards + punttouchdowns + kicktouchdowns + puntFumbles + kickFumbles
+  return kickYards + kickTouchdowns + kickFumbles + puntYards + puntTouchdowns + puntFumbles
 }
 
 module.exports = calculateScore
